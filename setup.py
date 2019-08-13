21423d090ccf60d7b317f2205ffeac4c34c25541
Alexander Skvortsov <askvortsov@youthneuro.org>
2019-08-12 21:13:46 -0400

    Added download URL to pypi settings, removed quote inconsistency

--- setup.py
+++ setup.py
@@ -3,30 +3,31 @@ from setuptools import setup, find_packages
 
 from dynamic_forms import __version__
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
     name='dynamic-django-forms',
     version=__version__,
     description='JSON-Powered Dynamic Forms for Django',
-    keywords="django,dynamic,forms,json",
+    download_url='https://github.com/askvortsov1/dynamic-django-forms/archive/v0.1.0.tar.gz',
+    keywords='django,dynamic,forms,json',
     author='Alexander Skvortsov',
     author_email='sasha.skvortsov109@gmail.com',
-    maintainer="Alexander Skvortsov",
+    maintainer='Alexander Skvortsov',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     install_requires=[
         'django>=2.0'
         ],
     license='MIT License',
-    packages=find_packages(exclude=["tests*", "docs", "example"]),
+    packages=find_packages(exclude=['tests*', 'docs', 'example']),
     url='https://github.com/askvortsov1/dynamic-django-forms',
     zip_safe=False,
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
