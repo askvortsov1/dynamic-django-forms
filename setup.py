ac93e9b5e3135c4731e69e02f7fc4cf22daacbf5
Alexander Skvortsov <askvortsov@youthneuro.org>
2019-08-12 22:55:32 -0400

    Bumped version to 0.1.2, fixed templates not showing up

--- setup.py
+++ setup.py
@@ -6,25 +6,31 @@ from dynamic_forms import __version__
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+standard_exclude = ["*.py", "*.pyc", "*~", ".*", "*.bak", "Makefile"]
+standard_exclude_directories = [
+    ".*", "CVS", "_darcs", "./build",
+    "./dist", "EGG-INFO", "*.egg-info",
+    "./example"
+]
+
 setup(
     name='dynamic-django-forms',
     version=__version__,
     description='JSON-Powered Dynamic Forms for Django',
-    download_url='https://github.com/askvortsov1/dynamic-django-forms/archive/v0.1.0.tar.gz',
     keywords='django,dynamic,forms,json',
     author='Alexander Skvortsov',
     author_email='sasha.skvortsov109@gmail.com',
     maintainer='Alexander Skvortsov',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    include_package_data=True,
     install_requires=[
         'django>=2.0'
         ],
     license='MIT License',
-    packages=find_packages(exclude=['tests*', 'docs', 'example']),
+    packages=find_packages(exclude=['tests', 'docs', 'example']),
     url='https://github.com/askvortsov1/dynamic-django-forms',
     zip_safe=False,
-    include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
